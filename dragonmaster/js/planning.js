841fadd2e88637a65a68c3041dc997b685883aa4
Alec Prettyman <prettale4@gmail.com>
2022-12-01 23:52:58 -0700

    added collapsable description to monsters list

--- dragonmaster/js/planning.js
+++ dragonmaster/js/planning.js
@@ -7,10 +7,36 @@ const displayMonsters = (monstersArray) => {
     const monsterDiv = document.createElement('div')
     monsterDiv.classList.add('monster-selector')
 
-    monsterDiv.innerHTML = `<p>${monster.name}</p>
+    const accordion = document.createElement('div')
+    accordion.classList.add('accordion')
+
+    accordion.innerHTML = `<p>${monster.name}</p>
     <button>Add</button>`
 
+    const panel = document.createElement('div')
+    panel.classList.add('panel')
+
+    panel.innerHTML = '<p>Specs and description from API</p>'
+    
+    monsterDiv.appendChild(accordion)
+    monsterDiv.appendChild(panel)
+
     monsterSelection.appendChild(monsterDiv)
+
+    accordion.addEventListener("click", function() {
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      monsterDiv.classList.toggle("active");
+
+      /* Toggle between hiding and showing the active panel */
+      if (panel.style.maxHeight) {
+        panel.style.display = 'none'
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.display = 'block'
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    })
   });
 
   const collapseArrows = document.querySelectorAll('.collapse')
